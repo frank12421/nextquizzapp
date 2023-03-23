22937481e1eefce6869115b319c6729cd70e7a3d
Frank Böker <boeker.frank@gmail.com>
2023-03-23 06:58:45 +0100

    final js Show/hide Answer and Button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
-// ---- Card Bookmark click Start ------
+// --- init hide answer / show Button ---
+const buttonHideAnswer = document.querySelector('[data-js="buttonHideAnswer"]');
+const buttonShowAnswer = document.querySelector('[data-js="buttonShowAnswer"]');
+const cardAnswerText = document.querySelector('[data-js="cardAnswerText"]');
+
+buttonHideAnswer.style.display = "none";
+cardAnswerText.style.display = "none";
+
+// eventlistener Show Answer
+
+buttonShowAnswer.addEventListener("click", (event) => {
+  //   console.log("yes");
+  buttonShowAnswer.style.display = "none";
+  cardAnswerText.style.display = "";
+  buttonHideAnswer.style.display = "";
+});
+
+// eventListener Hide cardAnswerText
+
+buttonHideAnswer.addEventListener("click", (event) => {
+  //   console.log("yes");
+  cardAnswerText.style.display = "none";
+  buttonShowAnswer.style.display = "";
+  buttonHideAnswer.style.display = "none";
+});
+
+// ---- Card Bookmark toogle Start ------
 const bookmarkButtonCard = document.querySelector(
   '[data-js="bookmarkButtonCard"]'
 );
@@ -9,4 +35,4 @@ const bookmarkButtonSvg = document.querySelector(
 bookmarkButtonCard.addEventListener("click", () => {
   bookmarkButtonSvg.classList.toggle("bookmark__button__svg--marked");
 });
-// ---- Card Bookmark click End ------
+// ---- Card Bookmark toogle End ------
