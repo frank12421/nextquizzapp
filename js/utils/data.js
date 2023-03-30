a92d6e8aeb802f597fbcb2d2eab2208a7c179632
Frank Böker <boeker.frank@gmail.com>
2023-03-30 19:53:45 +0200

    card elements moved to new js file

---
+++ js/utils/data.js
@@ -0,0 +1,16 @@
+export const quizz = [
+  {
+    frage: "Was ist die antwort auf das Universum  und alles andere?",
+    antwort: "42",
+    bookmark: false,
+    tag: ["all", "html", "css"],
+  },
+  {
+    frage:
+      "1 - Diese Frage ist eigentlich keine wirkliche Frage, sondern lediglich ein Text der die vollen 150 Zeichen ausnutzt und dann ohne Frage einfach unvollend",
+    antwort:
+      "1 - Und hier folgt die Antwort - ebenfalls nicht zu kurz, nicht zu lang - damit sie gut zu merken ist.",
+    bookmark: true,
+    tag: ["max", "html", "css"],
+  },
+];
