f0266bacf618468784eb2403f864309a6b423263
Frank Böker <boeker.frank@gmail.com>
2023-04-01 02:36:55 +0200

    js for new form works

--- js/new-question.js
+++ js/new-question.js
@@ -11,12 +11,50 @@ export function addNewQuestion() {
 function addForm(maxLength) {
   const form = document.createElement("form");
   form.append(addTextArea("newQestion", "150", " Post your Question here."));
-  form.append(addTextArea("newQestion", "150", " Post your ANSWER here."));
-
+  form.append(addTextArea("newAnswer", "150", " Post your ANSWER here."));
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  });
+  form.append(addSubmitButton());
   return form;
 }
+
+function addSubmitButton() {
+  const htmlstructure = cardContainer();
+  const button = document.createElement("button");
+  button.type = "submit";
+  button.textContent = "Drück mich";
+  button.addEventListener("submit", (event) => {
+    //event.preventDefault();
+    console.log("click submit-form");
+
+    //const newCard = document.createElement("article");
+
+    // newCard.classList.add("card");
+    // const newQuestion = document.createElement("p");
+    // newQuestion.classList.add("card__question");
+    // newQuestion.textContent = newForm.newQuestion.value;
+
+    // const newAnswer = document.createElement("p");
+    // newAnswer.classList.add("card__answer");
+    // newAnswer.textContent = newForm.newAnswer.value;
+
+    // const newTag = document.createElement("p");
+    // newTag.textContent = newForm.inputTag.value;
+
+    //     newCard.append(newQuestion);
+    //     newCard.append(newAnswer);
+    //     newCard.append(newTag);
+    //     newMain.append(newCard);
+  });
+
+  htmlstructure.append(button);
+  return htmlstructure;
+}
+
 function addTextArea(name, maxlength, labelName) {
-  const section = document.createElement("section");
+  const section = cardContainer();
   const label = document.createElement("label");
   const charCounter = document.createElement("p");
   charCounter.textContent = maxlength + " character left";
